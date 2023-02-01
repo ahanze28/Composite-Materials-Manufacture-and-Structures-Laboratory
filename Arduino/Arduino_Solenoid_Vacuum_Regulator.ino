5fbd857713128208d9cadc475ffb618eca05e981
Andrew Hanze <124201904+ahanze28@users.noreply.github.com>
2023-02-01 16:31:03 -0700

    Create Arduino_Solenoid_Vacuum_Regulator.ino
    Arduino-controlled vacuum regulator for ABB robotic 3D printer. Regulates flow of thermoplastic pellets by opening and closing vacuum tunnel with solenoid.

---
+++ Arduino/Arduino_Solenoid_Vacuum_Regulator.ino
@@ -0,0 +1,20 @@
+
+/* Solenoid System Test */
+
+int solenoidPin = 7;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(solenoidPin, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(solenoidPin, HIGH);
+  delay(7000);
+  digitalWrite(solenoidPin, LOW);
+  delay(7000);
+
+}
